e23d2174f109a552b07137308662e067f301ed60
Gusta <Gusta@GUSATVO>
2023-04-11 19:42:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto ferneko 2/Controllers/FornecedoresController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuProjeto.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly List<Fornecedor> _fornecedores = new List<Fornecedor>
+        {
+            new Fornecedor { Id = 1, Nome = "Fornecedor 1", Telefone = "1234567890" },
+            new Fornecedor { Id = 2, Nome = "Fornecedor 2", Telefone = "0987654321" },
+            new Fornecedor { Id = 3, Nome = "Fornecedor 3", Telefone = "5555555555" }
+        };
+
+        // GET: Fornecedores
+        public ActionResult Index()
+        {
+            return View(_fornecedores);
+        }
+
+        // GET: Fornecedores/Cadastrar
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // POST: Fornecedores/Cadastrar
+        [HttpPost]
+        public ActionResult Cadastrar(Fornecedor fornecedor)
+        {
+            fornecedor.Id = _fornecedores.Count + 1;
+            _fornecedores.Add(fornecedor);
+            return RedirectToAction("Index");
+        }
+    }
+
+    internal class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Projeto ferneko 2/Controllers/ProdutosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuProjeto.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly List<Produto> _produtos = new List<Produto>
+        {
+            new Produto { Id = 1, Nome = "Produto 1", Preco = 10.0m },
+            new Produto { Id = 2, Nome = "Produto 2", Preco = 20.0m },
+            new Produto { Id = 3, Nome = "Produto 3", Preco = 30.0m }
+        };
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(_produtos);
+        }
+
+        // GET: Produtos/Cadastrar
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Cadastrar
+        [HttpPost]
+        public ActionResult Cadastrar(Produto produto)
+        {
+            produto.Id = _produtos.Count + 1;
+            _produtos.Add(produto);
+            return RedirectToAction("Index");
+        }
+    }
+
+    internal class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Produto(string nome)
+        {
+            Nome = nome;
+        }
+
+        public decimal Preco { get; set; }
+    }
+}
